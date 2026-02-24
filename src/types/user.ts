fd473873b129b2a10f9947900443b7ee76d2f6b8
EcchiGrill <dmitry.oborsky@gmail.com>
2026-02-24 12:30:46 +0200

    :recycle: Removed orders field in user

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,11 +1,7 @@
-import type { Order } from './order';
-
 export interface User {
   id: string;
   username: string;
   email: string;
   avatar: string;
-  orders: Order[];
   isAdmin: boolean;
-  accessToken: string;
 }

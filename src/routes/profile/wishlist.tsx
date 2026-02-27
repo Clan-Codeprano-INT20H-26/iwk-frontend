1ddfff9330acdbc2e72adabe1124fc98e9d95db8
rubanAlexUa <ruban.Alex.Y.P@gmail.com>
2026-02-27 20:03:14 +0200

    :zap:updated recently reviewed and wishlist pages

---
+++ src/routes/profile/wishlist.tsx
@@ -0,0 +1,26 @@
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { createFileRoute } from '@tanstack/react-router';
+import { PageLoader } from '@/components/PageLoader';
+import { useWishlist } from '@/lib/hooks/useWishlist';
+import { WishCard } from '@/components/WishCard';
+
+const WishlistPage = () => {
+  const {wishes} = useWishlist()
+
+  return (
+    <>
+      <Typography variant='h3' sx={{mb:'70px'}}>My Wishlist</Typography>
+      <Stack gap={5} flexDirection='row' flexWrap='wrap'>
+        {wishes.map(({id, name, price, images}, index) => (
+          <WishCard key={index} id={id} name={name} price={price} image={images[0]}/>
+        ))}
+      </Stack>
+    </>
+  );
+};
+
+export const Route = createFileRoute('/profile/wishlist')({
+  component: WishlistPage,
+  pendingComponent: PageLoader,
+});

897332b9e8a4641f8b2abceb35e0de295d444daa
EcchiGrill <dmitry.oborsky@gmail.com>
2026-02-28 20:14:25 +0200

    :sparkles: Server error loader

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,11 +1,14 @@
 import Backdrop from '@mui/material/Backdrop';
 import { Loader } from './ui/Loader';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
 
 interface PageLoaderProps {
   open?: boolean;
+  title?: string;
 }
 
-export const PageLoader = ({ open = true }: PageLoaderProps) => {
+export const PageLoader = ({ open = true, title }: PageLoaderProps) => {
   return (
     <Backdrop
       open={open}
@@ -13,7 +16,14 @@ export const PageLoader = ({ open = true }: PageLoaderProps) => {
         zIndex: (theme) => theme.zIndex.drawer + 1,
       }}
     >
-      <Loader size={120} />
+      <Stack alignItems="center" gap={6}>
+        <Loader size={150} />
+        {title && (
+          <Typography variant="h4" color="primary.light">
+            {title}
+          </Typography>
+        )}
+      </Stack>
     </Backdrop>
   );
 };

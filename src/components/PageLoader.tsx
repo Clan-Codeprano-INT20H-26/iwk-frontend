d07e53fc84439dce24991f4701da029c7a4d9a64
EcchiGrill <dmitry.oborsky@gmail.com>
2026-03-01 13:16:27 +0200

    Revert ":sparkles: Server error loader"
    This reverts commit 2e6577a015c92be2a246d3540780605fa4b0a422.

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,14 +1,11 @@
 import Backdrop from '@mui/material/Backdrop';
 import { Loader } from './ui/Loader';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
 
 interface PageLoaderProps {
   open?: boolean;
-  title?: string;
 }
 
-export const PageLoader = ({ open = true, title }: PageLoaderProps) => {
+export const PageLoader = ({ open = true }: PageLoaderProps) => {
   return (
     <Backdrop
       open={open}
@@ -16,14 +13,7 @@ export const PageLoader = ({ open = true, title }: PageLoaderProps) => {
         zIndex: (theme) => theme.zIndex.drawer + 1,
       }}
     >
-      <Stack alignItems="center" gap={6}>
-        <Loader size={150} />
-        {title && (
-          <Typography variant="h4" color="primary.light">
-            {title}
-          </Typography>
-        )}
-      </Stack>
+      <Loader size={120} />
     </Backdrop>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import { NotFound } from './components/NotFound';
-import { PageLoader } from './components/PageLoader';
 
 const router = createRouter({
   routeTree,
   defaultNotFoundComponent: NotFound,
-  defaultErrorComponent: () => (
-    <PageLoader title={'Please wait while we are loading the page...'} />
-  ),
   scrollRestoration: true,
 });
 
